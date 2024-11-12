621ea774245acf306cb4a5253b9d107f433071ab
arron <arrondunne@gmail.com>
2024-11-12 11:04:40 +1100

    removing stuff

--- textbook/chapter-1/1-1/sandbox.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket
-(require berkeley)
-
-(if (= 0 0)
-    (+ 3 2)
-    (- 3 2))
--- textbook/chapter-1/1-3/sum-iter.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(define (sum term a next b)
-  (define (iter a result)
-    (if (a > b)
-        result
-        (iter (next a) (+ result (term a))))
-    (iter a 0.0)))

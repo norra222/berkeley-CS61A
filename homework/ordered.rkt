a4182b4789b242193745614a4617555b9d1c91ad
arron <arrondunne@gmail.com>
2024-11-12 11:01:43 +1100

    removing unwanted files

--- homework/ordered.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require berkeley)
-
-(define (ordered? nums)
-  (if (empty? (bf nums))
-      #t
-      (and (<= (first nums) (first (bf nums))) (ordered? (bf nums)))))
-
-(ordered? '(1 100 101 103 1000 100000 100))
